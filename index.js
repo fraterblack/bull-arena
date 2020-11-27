9be1790666abad6e14e0153731fd4f58a3275373
fraterblack <fraterblack@bck.com.br>
2020-11-27 20:24:01 -0300

    Create project

---
+++ index.js
@@ -0,0 +1,45 @@
+const Arena = require('bull-arena');
+const Bull = require('bull');
+const express = require('express');
+
+const app = express();
+
+const redis = {
+    port: 6379,
+    host: 'localhost',
+};
+
+const arenaConfig = Arena(
+    {
+        Bull,
+        queues: [
+            {
+                type: 'bull',
+                name: "timesheet_main_queue",
+                hostId: "Main Queue",
+                redis,
+            },
+            {
+                type: 'bull',
+                name: "timesheet_calculation_queue",
+                hostId: "Calculation Queue",
+                redis,
+            },
+            {
+                type: 'bull',
+                name: "timesheet_reprocessing_queue",
+                hostId: "Reprocessing Queue",
+                redis,
+            },
+        ],
+    },
+    {
+        basePath: '/',
+        disableListen: true
+    });
+
+app.use('/', arenaConfig);
+
+app.listen(5678, function () {
+    console.log('Listening on port 5678');
+});
